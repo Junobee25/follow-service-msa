4d5d4a80d4242b254693d4f69bedc96afd4ceee2
Junobee25 <wnsdhqo@naver.com>
2024-02-08 22:03:40 +0900

    #4 - FeignClient 으로 변경
    기존에 마이크로 서비스 간 통신을 위해 RestTemplate을 사용했지만, 개발의 편의를 위해 FeignClient로 변경

--- src/main/java/com/hanghae/followservice/FollowServiceApplication.java
+++ src/main/java/com/hanghae/followservice/FollowServiceApplication.java
@@ -3,19 +3,15 @@ package com.hanghae.followservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class FollowServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FollowServiceApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
 }
